3b28329ca1e848e70bec0323679be2fadf9f1a5a
Anton Zaviyalov <zavanton123@gmail.com>
2019-11-16 11:37:35 +0300

    data passed unsafely via bundle

--- app/src/main/java/com/example/demonav/DetailFragment.kt
+++ app/src/main/java/com/example/demonav/DetailFragment.kt
@@ -1,11 +1,11 @@
 package com.example.demonav
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -14,5 +14,12 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val lastName = arguments?.getString(getString(R.string.lastName)) ?: "empty"
+        tvDetail.text = lastName
     }
 }
